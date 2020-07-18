655dcf470372900253fbf843b51a7db598306a81
Mikhail Bakalov <mike@mbakalov.com>
2020-07-18 17:49:51 -0400

    Get rid of the TEAMCITY_PROCESS_FLOW_ID env variable to get the "PID" behavior for assigning flowIds for parallel nunit processes.

--- .teamcity/Invoke-NUnitParallel.ps1
+++ .teamcity/Invoke-NUnitParallel.ps1
@@ -30,12 +30,18 @@ $block = {
     $dbName = "BlogContext$bucketId"
     $outputDir = ".\output\$bucketId"
 
-    Write-Host "DEBUG: TEAMCITY_PROCESS_FLOW_ID='$env:TEAMCITY_PROCESS_FLOW_ID'"
     & .\packages\NUnit.ConsoleRunner.3.11.1\tools\nunit3-console.exe `
         .\FrameworkApp.Tests\bin\Debug\FrameworkApp.Tests.dll `
         --testlist=$testFile --testparam:DbName=$dbName --work=$outputDir `
         --teamcity
 }
+
+# Want to use PID of each individual nunit3-console.exe process to correctly assign
+# "flowId" for TeamCity service messages, see https://github.com/nunit/teamcity-event-listener/issues/66
+# The PID works only if the "TEAMCITY_PROCESS_FLOW_ID" env variable is not set.
+# Not sure why it is getting set in my TeamCity instance, but removing it in the script
+# explicitly to get the "PID" behavior
+Remove-Item $env:TEAMCITY_PROCESS_FLOW_ID
 
 $jobs = @()
 $currentDir = Get-Location
