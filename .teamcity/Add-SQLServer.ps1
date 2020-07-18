d658fbd83b65b5fda1adc4286dc6c8293f9750a5
Mikhail Bakalov <mike@mbakalov.com>
2020-07-18 12:05:21 -0400

    SQL Server Windows container is Windows Server 2016 kernel which is incompatible with the 2019 host. Have to use Hyper-V mode when running the container.

--- .teamcity/Add-SQLServer.ps1
+++ .teamcity/Add-SQLServer.ps1
@@ -1,8 +1,11 @@
 $saPassword = "h4rdc0dedThr0wAw4yPwd!"
 & docker pull microsoft/mssql-server-windows-developer
-& docker run --name tcdemo-sql-001 -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$saPassword" -p 1444:1433 -d microsoft/mssql-server-windows-developer
-
-Start-Sleep -Seconds 10 # Let the SQL Server start...
+& docker run `
+    --name tcdemo-sql-001 `
+    -e "ACCEPT_EULA=Y" `
+    -e "SA_PASSWORD=$saPassword" `
+    -p 1444:1433 -d --isolation=hyperv `
+    microsoft/mssql-server-windows-developer
 
 & docker exec -it tcdemo-sql-001 sqlcmd `
     -S localhost -U sa -P "$saPassword" `
