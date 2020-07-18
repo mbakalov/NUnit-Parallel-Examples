1ac5f5c8fc095c90dfba8ad621b7b3aa0e6a204d
Mikhail Bakalov <mike@mbakalov.com>
2020-07-18 09:00:16 -0400

    Support both Windows and Linux SQL Server container images.

--- src/FrameworkApp/run-docker.ps1
+++ src/FrameworkApp/run-docker.ps1
@@ -1,14 +1,24 @@
-# Setup Linux SQL Server to run tests against
+param([switch] $UseWindowsContainer)
+
 $saPassword = "h4rdc0dedThr0wAw4yPwd!"
-& docker pull mcr.microsoft.com/mssql/server
-& docker run --name tcdemo-sql-001 -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$saPassword" -p 1434:1433 -d mcr.microsoft.com/mssql/server
+
+if ($UseWindowsContainer) {
+    $image = "microsoft/mssql-server-windows-developer"
+    $sqlcmd = "sqlcmd"
+} else {
+    $image = "mcr.microsoft.com/mssql/server"
+    $sqlcmd = "/opt/mssql-tools/bin/sqlcmd"
+}
+
+& docker pull $image
+& docker run --name tcdemo-sql-001 -e "ACCEPT_EULA=Y" -e "sa_password=$saPassword" -p 1444:1433 -d $image
 
 Start-Sleep -Seconds 10 # Let the SQL Server start...
 
-& docker exec -it tcdemo-sql-001 /opt/mssql-tools/bin/sqlcmd `
+& docker exec -it tcdemo-sql-001 $sqlcmd `
     -S localhost -U sa -P "$saPassword" `
     -Q "CREATE LOGIN [testuser] WITH PASSWORD=N'testpassword', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF"
-& docker exec -it tcdemo-sql-001 /opt/mssql-tools/bin/sqlcmd `
+& docker exec -it tcdemo-sql-001 $sqlcmd `
     -S localhost -U sa -P "$saPassword" `
     -Q "ALTER SERVER ROLE [sysadmin] ADD MEMBER [testuser]"
 
