ad2cc3972dfb22bc4451844cc655c331dae3d157
Mikhail Bakalov <mike@mbakalov.com>
2020-07-18 12:38:41 -0400

    Workaround: add a bit of wait until SQL Server starts up.

--- .teamcity/Add-SQLServer.ps1
+++ .teamcity/Add-SQLServer.ps1
@@ -7,6 +7,8 @@ $saPassword = "h4rdc0dedThr0wAw4yPwd!"
     -p 1444:1433 -d --isolation=hyperv `
     microsoft/mssql-server-windows-developer
 
+Start-Sleep -Seconds 30 # Wait for SQL Server to start up (get rid of this later)
+
 & docker exec tcdemo-sql-001 sqlcmd `
     -S localhost -U sa -P "$saPassword" `
     -Q "CREATE LOGIN [testuser] WITH PASSWORD=N'testpassword', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF"
