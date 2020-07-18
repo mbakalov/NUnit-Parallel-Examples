0c98621e50441f35e5075f6a4e75118e4bf0673c
Mikhail Bakalov <mike@mbakalov.com>
2020-07-18 12:52:48 -0400

    Keep trying multiple times until SQL Server fully starts.

--- .teamcity/Add-SQLServer.ps1
+++ .teamcity/Add-SQLServer.ps1
@@ -7,11 +7,19 @@ $saPassword = "h4rdc0dedThr0wAw4yPwd!"
     -p 1444:1433 -d --isolation=hyperv `
     microsoft/mssql-server-windows-developer
 
-Start-Sleep -Seconds 30 # Wait for SQL Server to start up (get rid of this later)
-
-& docker exec tcdemo-sql-001 sqlcmd `
-    -S localhost -U sa -P "$saPassword" `
-    -Q "CREATE LOGIN [testuser] WITH PASSWORD=N'testpassword', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF"
-& docker exec tcdemo-sql-001 sqlcmd `
-    -S localhost -U sa -P "$saPassword" `
-    -Q "ALTER SERVER ROLE [sysadmin] ADD MEMBER [testuser]"
+for ($i = 0; $i -lt 20; $i++) {
+    & docker exec tcdemo-sql-001 sqlcmd `
+        -S localhost -U sa -P "$saPassword" `
+        -Q "CREATE LOGIN [testuser] WITH PASSWORD=N'testpassword', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF"
+    & docker exec tcdemo-sql-001 sqlcmd `
+        -S localhost -U sa -P "$saPassword" `
+        -Q "ALTER SERVER ROLE [sysadmin] ADD MEMBER [testuser]"
+    
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host "testuser created!"
+        break
+    } else {
+        Write-Host "Failed to create testuser, probably SQL Server hasn't started yet. Will retry in 15 seconds. Attempt $i/20"
+        Start-Sleep -Seconds 15
+    }
+}
