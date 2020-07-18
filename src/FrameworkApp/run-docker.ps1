38955c2da7031b6bde0131febed527f0e3711d79
Mikhail Bakalov <mike@mbakalov.com>
2020-07-18 15:10:05 -0400

    In non-ci script add ability to run tests in parallel using SQL Server container and display progress of running parallel jobs.

--- src/FrameworkApp/run-docker.ps1
+++ src/FrameworkApp/run-docker.ps1
@@ -1,4 +1,4 @@
-param([switch] $UseWindowsContainer)
+param($Buckets = 4, [switch] $UseWindowsContainer)
 
 $saPassword = "h4rdc0dedThr0wAw4yPwd!"
 
@@ -36,7 +36,58 @@ if (-not (Test-Path .\output))
     mkdir .\output
 }
     
-& msbuild .\FrameworkApp.sln /t:Build /p:FixtureCount=4 /p:TestsPerFixtureCount=2
+& msbuild .\FrameworkApp.sln /t:Build /p:FixtureCount=10 /p:TestsPerFixtureCount=10
 
+# Single process
+# & .\packages\NUnit.ConsoleRunner.3.11.1\tools\nunit3-console.exe `
+#     .\FrameworkApp.Tests\bin\Debug\FrameworkApp.Tests.dll --work=.\output
+
+# Multi-process
 & .\packages\NUnit.ConsoleRunner.3.11.1\tools\nunit3-console.exe `
-    .\FrameworkApp.Tests\bin\Debug\FrameworkApp.Tests.dll --work=.\output
+    .\FrameworkApp.Tests\bin\Debug\FrameworkApp.Tests.dll --explore=.\output\allTests.xml
+
+$testsXml = [xml] (gc .\output\allTests.xml)
+$fullNames = $testsXml.SelectNodes('//test-case') | select -ExpandProperty fullname
+$testCount = $fullNames.Count
+$perBucket = [int] [Math]::Ceiling($testCount / $Buckets)
+$bucket = 0
+for ($i = 0; $i -lt $testCount; $i += $perBucket) {
+    $subset = $fullNames[$i..($i + $perBucket - 1)]
+ 
+    $subset | Out-File ".\output\Tests_$bucket.txt" -Encoding ascii
+
+    $bucket++
+}
+
+$block = {
+    param($workDir, $bucketId)
+
+    Set-Location $workDir
+
+    $testFile = ".\output\Tests_$bucketId.txt"
+    $dbName = "BlogContext$bucketId"
+    $outputDir = ".\output\$bucketId"
+
+    & .\packages\NUnit.ConsoleRunner.3.11.1\tools\nunit3-console.exe `
+        .\FrameworkApp.Tests\bin\Debug\FrameworkApp.Tests.dll `
+        --testlist=$testFile --testparam:DbName=$dbName --work=$outputDir `
+        --teamcity
+}
+
+$jobs = @()
+$currentDir = Get-Location
+for ($i = 0; $i -lt $buckets; $i++) {
+    $jobs += Start-Job $block -Name "NUnit_TestJob_$i" -ArgumentList $currentDir, $i 
+}
+
+# Display progress will jobs are running
+while ($jobs.State -contains "Running") {
+    $jobs | Receive-Job
+    Start-Sleep -Seconds 10
+}
+
+# Display final output
+$jobs | Receive-Job
+
+# Cleanup jobs
+$jobs | Remove-Job
